8c67fa85f774408f9bb181e6811eb2dedf420ff3
Zhonggang Li <lizhonggang80@gmail.com>
2020-11-30 23:09:12 -0600

    Update regressionTree.py

--- regressionTree.py
+++ regressionTree.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 """
 @author: John Li, zli769@wisc.edu
+@regression Tree marine microplastic data
  """
-
-# -*- coding: utf-8 -*-
 
 from copy import copy
 import numpy as np
@@ -11,17 +9,8 @@ from numpy import ndarray
 from numpy.random import choice, seed
 import matplotlib.pyplot as plt
 
+# Node is used in regression tree which helps its built
 class Node:
-    """Node class to build tree leaves.
-    Attributes:
-        avg {float} -- prediction of label. (default: {None})
-        left {Node} -- Left child node.
-        right {Node} -- Right child node.
-        feature {int} -- Column index.
-        split {int} --  Split point.
-        mse {float} --  Mean square error.
-    """
-
     attr_names = ("avg", "left", "right", "feature", "split", "mse")
 
     def __init__(self, avg=None, left=None, right=None, feature=None, split=None, mse=None):
@@ -49,23 +38,13 @@ class Node:
 
     def copy(self, node):
         """Copy the attributes of another Node.
-        Arguments:
-            node {Node}
         """
-
         for attr_name in self.attr_names:
             attr = getattr(node, attr_name)
             setattr(self, attr_name, attr)
 
-
+# regression tree has root, depth and rules attributes
 class RegressionTree:
-    """RegressionTree class.
-    Attributes:
-        root {Node} -- Root node of RegressionTree.
-        depth {int} -- Depth of RegressionTree.
-        _rules {list} -- Rules of all the tree nodes.
-    """
-
     def __init__(self):
         self.root = Node()
         self.depth = 1
@@ -75,37 +54,24 @@ class RegressionTree:
         ret = []
         for i, rule in enumerate(self._rules):
             literals, avg = rule
-
             ret.append("Rule %d: " % i + ' | '.join(
                 literals) + ' => y_hat %.4f' % avg)
         return "\n".join(ret)
 
     @staticmethod
     def _expr2literal(expr: list) -> str:
-        """Auxiliary function of get_rules.
-        Arguments:
-            expr {list} -- 1D list like [Feature, op, split].
-        Returns:
-            str
-        """
-
         feature, operation, split = expr
         operation = ">=" if operation == 1 else "<"
         return "Feature%d %s %.4f" % (feature, operation, split)
 
+    # to search for all split rules, use bfs to do level travese
     def get_rules(self):
-        """Get the rules of all the tree nodes.
-            Expr: 1D list like [Feature, op, split].
-            Rule: 2D list like [[Feature, op, split], label].
-            Op: -1 means less than, 1 means equal or more than.
-        """
-
         # Breadth-First Search.
-        que = [[self.root, []]]
+        queue = [[self.root, []]]
         self._rules = []
 
-        while que:
-            node, exprs = que.pop(0)
+        while queue:
+            node, exprs = queue.pop(0)
 
             # Generate a rule when the current node is leaf node.
             if not (node.left or node.right):
@@ -117,33 +83,20 @@ class RegressionTree:
             if node.left:
                 rule_left = copy(exprs)
                 rule_left.append([node.feature, -1, node.split])
-                que.append([node.left, rule_left])
+                queue.append([node.left, rule_left])
 
             # Expand when the current node has right child.
             if node.right:
                 rule_right = copy(exprs)
                 rule_right.append([node.feature, 1, node.split])
-                que.append([node.right, rule_right])
+                queue.append([node.right, rule_right])
 
     @staticmethod
+    # to get all mse of label and do split
     def _get_split_mse(col: ndarray, label: ndarray, split: float) -> Node:
-        """Calculate the mse of label when col is splitted into two pieces.
-        MSE as Loss fuction:
-        y_hat = Sum(y_i) / n, i <- [1, n]
-        Loss(y_hat, y) = Sum((y_hat - y_i) ^ 2), i <- [1, n]
-        --------------------------------------------------------------------
-        Arguments:
-            col {ndarray} -- A feature of training data.
-            label {ndarray} -- Target values.
-            split {float} -- Split point of column.
-        Returns:
-            Node -- MSE of label and average of splitted x
-        """
-
         # Split label.
         label_left = label[col < split]
         label_right = label[col >= split]
-
         # Calculate the means of label.
         avg_left = label_left.mean()
         avg_right = label_right.mean()
@@ -156,19 +109,9 @@ class RegressionTree:
         node = Node(split=split, mse=mse)
         node.left = Node(avg_left)
         node.right = Node(avg_right)
-
         return node
-
+    # to choose the best split feature and cutoff
     def _choose_split(self, col: ndarray, label: ndarray) -> Node:
-        """Iterate each xi and split x, y into two pieces,
-        and the best split point is the xi when we get minimum mse.
-        Arguments:
-            col {ndarray} -- A feature of training data.
-            label {ndarray} -- Target values.
-        Returns:
-            Node -- The best choice of mse, split point and average.
-        """
-
         # Feature cannot be splitted if there's only one unique element.
         node = Node()
         unique = set(col)
@@ -179,91 +122,64 @@ class RegressionTree:
         unique.remove(min(unique))
 
         # Get split point which has min mse.
-        ite = map(lambda x: self._get_split_mse(col, label, x), unique)
-        node = min(ite, key=lambda x: x.mse)
-
+        item = map(lambda x: self._get_split_mse(col, label, x), unique)
+        node = min(item, key=lambda x: x.mse)
         return node
 
+    # choose the best feature
     def _choose_feature(self, data: ndarray, label: ndarray) -> Node:
-        """Choose the feature which has minimum mse.
-        Arguments:
-            data {ndarray} -- Training data.
-            label {ndarray} -- Target values.
-        Returns:
-            Node -- feature number, split point, average.
-        """
-
         # Compare the mse of each feature and choose best one.
-        _ite = map(lambda x: (self._choose_split(data[:, x], label), x),
+        item = map(lambda x: (self._choose_split(data[:, x], label), x),
                    range(data.shape[1]))
-        ite = filter(lambda x: x[0].split is not None, _ite)
+        item = filter(lambda x: x[0].split is not None, item)
 
-        # Return None if no feature can be splitted.
-        node, feature = min(
-            ite, key=lambda x: x[0].mse, default=(Node(), None))
+        node, feature = min(item, key=lambda x: x[0].mse, default=(Node(), None))
         node.feature = feature
-
         return node
 
-    def fit(self, data: ndarray, label: ndarray, max_depth=5, min_samples_split=2):
-        """Build a regression decision tree.
-        Note:
-            At least there's one column in data has more than 2 unique elements,
-            and label cannot be all the same value.
-        Arguments:
-            data {ndarray} -- Training data.
-            label {ndarray} -- Target values.
-        Keyword Arguments:
-            max_depth {int} -- The maximum depth of the tree. (default: {5})
-            min_samples_split {int} -- The minimum number of samples required
-            to split an internal node. (default: {2})
-        """
+    # this is the main function to put data into the tree, and get tree split
+    # default depth is 10
+    def fit(self, data: ndarray, label: ndarray, max_depth=10, min_samples_split=2):
 
         # Initialize with depth, node, indexes.
         self.root.avg = label.mean()
-        que = [(self.depth + 1, self.root, data, label)]
+        queue = [(self.depth + 1, self.root, data, label)]
 
         # Breadth-First Search.
-        while que:
-            depth, node, _data, _label = que.pop(0)
+        while queue:
+            depth, node, _data, _label = queue.pop(0)
 
             # Terminate loop if tree depth is more than max_depth.
             if depth > max_depth:
                 depth -= 1
                 break
-
             # Stop split when number of node samples is less than
             # min_samples_split or Node is 100% pure.
             if len(_label) < min_samples_split or all(_label == label[0]):
                 continue
 
             # Stop split if no feature has more than 2 unique elements.
-            _node = self._choose_feature(_data, _label)
-            if _node.split is None:
+            nnode = self._choose_feature(_data, _label)
+            if nnode.split is None:
                 continue
 
             # Copy the attributes of _node to node.
-            node.copy(_node)
+            node.copy(nnode)
 
             # Put children of current node in que.
             idx_left = (_data[:, node.feature] < node.split)
             idx_right = (_data[:, node.feature] >= node.split)
-            que.append(
+            queue.append(
                 (depth + 1, node.left, _data[idx_left], _label[idx_left]))
-            que.append(
+            queue.append(
                 (depth + 1, node.right, _data[idx_right], _label[idx_right]))
 
         # Update tree depth and rules.
         self.depth = depth
         self.get_rules()
 
+    # when there is only one vector can be predicted
     def predict_one(self, row: ndarray) -> float:
-        """Auxiliary function of predict.
-        Arguments:
-            row {ndarray} -- A sample of testing data.
-        Returns:
-            float -- Prediction of label.
-        """
         node = self.root
         while node.left and node.right:
             if row[node.feature] < node.split:
@@ -273,16 +189,8 @@ class RegressionTree:
 
         return node.avg
 
+    # this is an extension of the predict_one function
     def predict(self, data: ndarray) -> ndarray:
-        """Get the prediction of label.
-
-        Arguments:
-            data {ndarray} -- Testing data.
-
-        Returns:
-            ndarray -- Prediction of label.
-        """
-
         return np.apply_along_axis(self.predict_one, 1, data)
 
 def load_data(filename):
@@ -315,7 +223,6 @@ def train_test_split(data, label=None, prob=0.95, random_state=None):
     # Set random state.
     if random_state is not None:
         seed(random_state)
-
     # Split data
     n_rows, _ = data.shape
     k = int(n_rows * prob)
@@ -331,7 +238,6 @@ def train_test_split(data, label=None, prob=0.95, random_state=None):
         ret = (data_train, data_test, label_train, label_test)
     else:
         ret = (data_train, data_test)
-
     # Cancel random state.
     if random_state is not None:
         seed(None)
@@ -377,6 +283,7 @@ def evalTree(filename):
     plt.ylabel("Goodness of Fitness (R2)")
     plt.show()
     return
+
 def testCase(filename,tree):
     predictions = []
     data = np.loadtxt(filename, skiprows=1, delimiter=',')
@@ -396,9 +303,12 @@ def testCase(filename,tree):
     return predictions
 
 if __name__ == "__main__":
+    # construct of regression tree based on marine data
     regTree = regressionTreeConstruct('Marine_Clean.csv', 10)
 
+    # evalTree to check all possible tree depth, and choose the best depth
     evalTree('Marine_Clean.csv')
 
+    # can use testCaseFunction to do prediction based on previously tree, and plot the predictions
     predictions = testCase('TestCase.csv',regTree)
 
