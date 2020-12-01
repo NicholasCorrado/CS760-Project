0aea1ebbceccda139679f8e19fb4cf860f13b334
Zhonggang Li <lizhonggang80@gmail.com>
2020-12-01 08:06:42 -0600

    Update main.py
    add regression tree running code in main.py

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from regressionTree import *
 
 
 if __name__ == "__main__":
-    pass
+    
+	
+	# regression tree	
+    regTree = regressionTreeConstruct('Marine_Clean.csv', 10) # construct of regression tree based on marine data    
+    evalTree('Marine_Clean.csv') # evalTree to check possible tree depth,choose optimal depth with goodness of fit    
+    predictions = testCase('TestCase.csv',regTree) # predict based on reg tree, and plot the predictions 
