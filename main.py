2ac79577f47e598531f0681bc76c66fc4c227ff3
Zhonggang Li <lizhonggang80@gmail.com>
2020-12-04 17:28:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from linearRegression import *
 from regressionTree import *
 
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__":    
 	
-	# regression tree	
-    regTree = regressionTreeConstruct('Marine_Clean.csv', 10) # construct of regression tree based on marine data    
-    evalTree('Marine_Clean.csv') # evalTree to check possible tree depth,choose optimal depth with goodness of fit    
-    predictions = testCase('TestCase.csv',regTree) # predict based on reg tree, and plot the predictions 
+    # construct of regression tree based on marine data
+    regTree = regressionTreeConstruct('Marine_Clean.csv', 10)
+    # evalTree to check all possible tree depth, and choose the best depth
+    r2_train, r2_test , MSE_train_tree = evalTree('Marine_Clean.csv')
+    print("r2 goodness of fit is %.2f" %r2_train)
+    print("MSE for training set is %.2f" %MSE_train_tree)
+    # can use testCaseFunction to do prediction based on previously tree, and plot the predictions
+    predictions, MSE_test_tree = testCase('TestCase.csv',regTree)
+    print("MSE for testing set is %.2f" % MSE_test_tree)
