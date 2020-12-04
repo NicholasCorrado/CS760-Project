e2e68268880282d5e45532bc226d6e72e03dc7d7
Dan Kiel <dannyj.kiel@gmail.com>
2020-12-04 16:23:40 -0600

    Added significance testing for linear regression.

--- linearRegression.py
+++ linearRegression.py
@@ -1,15 +1,20 @@
 import math
 import numpy as np
 import random as rand
+import scipy.stats
 
 
 class LinearRegression:
 
     def __init__(self, x, y):
-        self.x = x
+        # Including intercept, shape is nxd+1
+        self.x = self.__add_intercept(x)
         self.y = y
-        self.n, self.d = self.x.shape
-        self.theta_hat = self.learn(self.x, self.y)
+        (self.n, self.d) = self.x.shape
+        self.theta_hat = self.learn()
+
+    def get_theta_hat(self):
+        return self.theta_hat
 
     def __add_intercept(self, x):
         if len(np.shape(x)) == 1:
@@ -22,19 +27,28 @@ class LinearRegression:
             )
         return x
 
-    def learn(self, x, y):
-
-        # Including intercept, shape is nxd+1
-        x = self.__add_intercept(x)
-
+    def learn(self, x=None, y=None):
+        if x is None:
+            x = self.x
+        if y is None:
+            y = self.y
         # MSE estimation of theta_hat
         return np.linalg.inv(x.T @ x) @ x.T @ y
 
-    def mean_squared_error(self, x, y, theta_hat):
-        return np.sum((y-self.predict(x, theta_hat))**2)/x.shape[0]
+    def mean_squared_error(self, x=None, y=None, theta_hat=None):
+        if x is None:
+            x = self.x
+        if y is None:
+            y = self.y
+        if theta_hat is None:
+            theta_hat = self.theta_hat
+        return np.sum((y-self.predict(x, self.theta_hat))**2)/x.shape[0]
 
-    def predict(self, x, theta_hat):
-        return self.__add_intercept(x) @ theta_hat
+    def predict(self, x, theta_hat=None):
+        if theta_hat is None:
+            theta_hat = self.theta_hat
+            x = self.__add_intercept(x)
+        return x @ theta_hat
 
     def kfold(self, k=10, seed=None):
         # Set seed for repeatibility.
@@ -67,6 +81,23 @@ class LinearRegression:
 
         return total_mse/k
 
+    def get_significance(self, alpha=0.05):
+
+        half = y-(self.x @ self.theta_hat)
+        sigma2 = (half.T @ half)/self.n
+
+        # only 1-alpha to work with this package
+        inv_tail = scipy.stats.chi2(1).ppf(1-alpha)
+
+        covar_theta = sigma2*np.linalg.inv(self.x.T@self.x)
+
+        results = []
+        for i in range(len(self.theta_hat)):
+            results.append(
+                ((self.theta_hat[i]**2)/covar_theta[i][i])-inv_tail)
+
+        return np.array(results)
+
 
 if __name__ == "__main__":
     np.set_printoptions(edgeitems=20, linewidth=100000,
@@ -81,21 +112,18 @@ if __name__ == "__main__":
     x = allData[:, :5]
 
     lg = LinearRegression(x, y)
-    theta_hat = lg.theta_hat
-    print("Theta hat {0}".format(theta_hat))
+    print("Theta hat {0}".format(lg.get_theta_hat()))
 
-    new_x = np.array([1.43683E+12, 73.62611667, -81.36758333, 0, 1.55])
-    print(lg.predict(new_x, theta_hat))
+    print("MSE for dataset: {0}".format(
+        lg.mean_squared_error()))
 
-    new_x = np.array([[1.43683E+12, 73.62611667, -81.36758333, 0, 1.55],
-                      [1.43683E+12, 73.62611667, -81.36758333, 0, 1.6]])
-    print(lg.predict(new_x, theta_hat))
+    print("Average MSE for k=10 fold CV: {0}".format(lg.kfold(seed=1)))
 
-    predictions = lg.predict(x, theta_hat)
-    min_index = np.argmin(abs(y-predictions))
-    print(min_index)
-    print(x[min_index, :])
+    significance = lg.get_significance()
 
-    print(lg.mean_squared_error(x, y, theta_hat))
-
-    print(lg.kfold(seed=1))
+    features = ["Date", "Latitude", "Longitude",
+                "Water Temperature", "Sample Volume", "Bias"]
+    print("Feature Significance Testing\n< 0 means insignificant, > 0 means significant.")
+    for i in range(len(features)):
+        print("At {}, feature {} is {}.".format(
+            significance[i], features[i], "significant" if significance[i] > 0 else "insignificant"))
