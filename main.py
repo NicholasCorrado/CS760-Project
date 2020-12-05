487a869490ab3a0053127c95ae98a31c23782ea6
Zhonggang Li <lizhonggang80@gmail.com>
2020-12-04 19:19:48 -0600

    UPDATED

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ from regressionTree import *
 
 
 if __name__ == "__main__":
+    input_file ='Marine_Clean.csv'
+    test_file = 'TestCase.csv'
     # Linear Regression
 
+	
+	
     # Regression Tree
     print('Generating Regression Tree')
-    regTree = regressionTreeConstruct('Marine_Clean.csv', 10)
+    regTree = regressionTreeConstruct(input_file, 8) # input parameters (filename,tree max depth)
     # evalTree to check all possible tree depth, and choose the best depth
     evalTree('Marine_Clean.csv')
-    MSE_train = getTreeMSE('Marine_Clean.csv',8)
+    MSE_train = getTreeMSE(input_file,8) # getTreeMSE(filename, optimized_depth)
     print("MSE for training set is %.2f" %MSE_train)
-    ave_MSE_train = crossValidation('Marine_Clean.csv',10)
+    ave_MSE_train = crossValidation(input_file,10)
     print("MSE for 10 fold cv is %.2f" %ave_MSE_train)
     # can use testCaseFunction to do prediction based on previously tree, and plot the predictions
-    predictions, MSE_test = testCase('TestCase.csv',regTree)
+    predictions, MSE_test = testCase(test_file,regTree) # testcase(test_file_name, regressionTree)
     print("MSE for testing set is %.2f" % MSE_test)
+	
+	
+	# kNN
--- regressionTree.py
+++ regressionTree.py
@@ -353,14 +353,19 @@ def testCase(filename,tree):
     return predictions, mse_test
 
 if __name__ == "__main__":
-    # construct of regression tree based on marine data
-    regTree = regressionTreeConstruct('Marine_Clean.csv', 10)
+    input_file ='Marine_Clean.csv'
+    test_file = 'TestCase.csv'
+    # Linear Regression
+
+    # Regression Tree
+    print('Generating Regression Tree')
+    regTree = regressionTreeConstruct(input_file, 8) # input parameters (filename,tree max depth)
     # evalTree to check all possible tree depth, and choose the best depth
     evalTree('Marine_Clean.csv')
-    MSE_train = getTreeMSE('Marine_Clean.csv',8)
+    MSE_train = getTreeMSE(input_file,8) # getTreeMSE(filename, optimized_depth)
     print("MSE for training set is %.2f" %MSE_train)
-    ave_MSE_train = crossValidation('Marine_Clean.csv',10)
+    ave_MSE_train = crossValidation(input_file,10)
     print("MSE for 10 fold cv is %.2f" %ave_MSE_train)
     # can use testCaseFunction to do prediction based on previously tree, and plot the predictions
-    predictions, MSE_test = testCase('TestCase.csv',regTree)
+    predictions, MSE_test = testCase(test_file,regTree) # testcase(test_file_name, regressionTree)
     print("MSE for testing set is %.2f" % MSE_test)
