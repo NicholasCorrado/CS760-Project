2fced3c4b57010a3743a372f9be6a475c0dea808
Nick Corrado <ncorrado@wisc.edu>
2020-12-06 16:41:50 -0600

    Minor code updates, started documentation

--- knn.py
+++ knn.py
@@ -7,6 +7,8 @@ def load():
             "C:\\Nicholas\\Graduate\\Courses\\cs760\\project\\Marine_Clean_no_missing_values.csv", 
             delimiter=',', usecols=(0,1,2,3,4,5), skiprows=1)
     
+    data = data[:200,:]
+    
     # Normalize features and target
     ranges = [np.max(col)-np.min(col) for col in data.T]
     mins = [np.min(col) for col in data.T]
@@ -14,23 +16,29 @@ def load():
 
     data = np.array([(data.T[i]-mins[i])/ranges[i] for i in range(len(data.T))]).T
     
+    
     return data
     
-# Distance function: l1 norm
+# Distance function: l2 norm
 def d(x1, x2):
-    return np.linalg.norm(x1 - x2, 1)
+    return np.linalg.norm(x1 - x2, 2)
 
 
 def plot(x, y):
     
+    plt.rcParams.update({'font.size': 28})
     plt.figure(figsize=(16,16))
     plt.plot(x, y)
-    plt.ylabel("average loss")
-    plt.xlabel("k")
-    plt.title("kNN average loss vs. k")
+    plt.ylabel("Average Loss (averaged over 5 runs of 10-fold validation)")
+    plt.xlabel("k (number of neighbors used to predict target)")
+    plt.title("kNN Average Loss vs. k")
     
       
 def distance_matrix(X):
+    '''
+    Compute a distance matrix D where D[i,j] corresponds to the distance
+    between X[i] and X[j]
+    '''
     
     n = len(X)
     distances = np.zeros(shape=(n,n))
@@ -42,6 +50,17 @@ def distance_matrix(X):
     return distances
 
 def compute_loss(D, Y, K, offset):
+    '''
+    
+    D : 2D array
+        distance matrix
+    Y : 1D array
+        true target values
+    K : int 
+        number of neighbors used to compute our target estimate yhat
+    offset : int
+        index indicating which 
+    '''
     
     sorted_indices = np.argsort(D, axis=1)
     knn_indices = sorted_indices[:,:K]
@@ -55,13 +74,39 @@ def compute_loss(D, Y, K, offset):
         loss += (yhat-y)**2
     return loss
 
+def get_block_of_distance_matrix(D, start, stop, n_subsets):
+    '''
+    Return a block/slice of the distance matrix D containing only the rows 
+    from start to stop (including start, excluding stop), excluding the columns 
+    from start to stop. 
+    
+    For example, say D is a 6x6 matrix, and let start=2 and stop=4. Then a 2x4
+    matrix will be returned. The matrix below has 1 in each entry that is 
+    included in the returned matrix and a 0 for all excluded entries. 
+    
+    0 0 0 0 0 0
+    0 0 0 0 0 0
+    1 1 0 0 1 1
+    1 1 0 0 1 1
+    0 0 0 0 0 0
+    0 0 0 0 0 0
+    '''
+    
+    Dsub = D[start:stop, :]
+    if n_subsets > 1:
+        Dsub = np.delete(Dsub, slice(start, stop), axis=1)
+    return Dsub
+    
+
 
 def k_fold_validation(D, X, Y):
-    
+    '''
+    Run 10-fold validation for all k between 1 and 200.
+    '''
     n = len(X)
     n_subsets = 10
     
-    Ks = np.arange(1,100, 1)
+    Ks = np.arange(1,200, 1)
     losses = np.zeros(len(Ks))
     
     subset_size = int(np.around(n/n_subsets))
@@ -75,17 +120,29 @@ def k_fold_validation(D, X, Y):
         for i in range(n_subsets):
             start = subset_boundaries[i]
             stop = subset_boundaries[i+1]
+            
+            Dsub = get_block_of_distance_matrix(D, start, stop, n_subsets)
         
-            Dsub = D[start:stop, :]
-            if n_subsets > 1:
-                Dsub = np.delete(Dsub, slice(start, stop), axis=1)
+
                 
-            loss += compute_loss(Dsub, Y, k, start)
+            loss += compute_loss(Dsub, Y, k, start)/(stop-start)
         losses[j] = loss/n_subsets
             
     return Ks, losses
     
 def compute_yhat(x, K, X, Y):
+    '''
+    Compute the target estimate for input vector x
+
+    x : 1D array
+        input feature vector for which we are estimating the target
+    k : int
+        number of neighbors used to compute our target estimate
+    X : 2D array
+        matrix containing all feature vectors in the dataset
+    Y : 1D array
+        all true target values in the dataset
+    '''
     
     n = len(X)
     distances = np.zeros(len(X))
@@ -98,7 +155,7 @@ def compute_yhat(x, K, X, Y):
     
     return np.average([Y[i] for i in knn_indices])    
 
-def test():
+def test(k):
     
     data = np.loadtxt(
         "C:\\Nicholas\\Graduate\\Courses\\cs760\\project\\TestCase.csv", 
@@ -122,7 +179,7 @@ def test():
     for i in range(len(Xtest)):
         x = Xtest[i]
         y = Ytest[i]        
-        yhat = compute_yhat(x, 27, X, Y)
+        yhat = compute_yhat(x, k, X, Y)
         Yhat[i] = yhat
         
     plt.figure(figsize=(16,16))
@@ -133,11 +190,41 @@ def test():
     plt.title("asdf")
         
     
+    
+def compute_training_loss(k, X, Y):
+    '''
+    Compute the empircal loss on the training data. The optimal k value 
+    should be used.
+    
+    k : int
+        number of neighbors used to compute our target estimate
+    X : 2D array
+        matrix containing all feature vectors in the dataset
+    Y : 1D array
+        all true target values in the dataset
+    '''
+    n = len(X)
+    
+    loss = 0
+    for i in range(n):
+        yhat = compute_yhat(X[i], k, X, Y)
+        loss += (Y[i] - yhat)**2
+        
+    return loss/n
+    
 
 if __name__ == "__main__":
     data = load()
     n = len(data)
-    test()
+    print(n)
+    test(21)
+    
+    X = data[:,:-1]
+    Y = data[:,-1]
+    l = compute_training_loss(28, X, Y)
+    print("training loss =", l)
+#    for k in range(1,50,5):
+#        test(k)
 
     n_trials = 1
     all_losses = []
@@ -155,7 +242,7 @@ if __name__ == "__main__":
         all_losses.append(losses)
        
     avg_losses = np.average(all_losses, axis=0)
-    print(np.argmin(avg_losses)+1)
-    
-    
     plot(Ks, avg_losses)
+    print(Ks)
+    print(avg_losses)
+    print(np.argmin(avg_losses)+1)
